f164149ff6c51cbd317bdf438122b5953c700321
arencho01 <a.vardanyan00101@gmail.com>
2024-10-04 12:17:54 +0300

    add connection with database and get data out of it

--- public/index.php
+++ public/index.php
@@ -1,3 +1,38 @@
 <?php
 
-echo 'Hello World!';
+//    header('Content-Type: text/html; charset=utf-8');
+
+    $pdo;
+    try {
+        $pdo = new PDO('mysql:host=mysql;dbname=my_db;charset=utf8mb4', 'user', '123456');
+    }
+    catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+
+    $articles = $pdo->query("SELECT * FROM articles");
+    $row = $articles->fetch();
+
+    while($article = $articles->fetch()) {
+        echo $article['title'] . '<br>';
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="../favicon.ico">
+
+    <title>Блог</title>
+</head>
+<body>
+    <h2>Hello world</h2>
+
+    <div>
+
+    </div>
+</body>
+</html>
